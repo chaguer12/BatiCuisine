2996ee84a2cc00f1323845a2b53185d0ab8083b6
zoolotoui <chag.med.amine@gmail.com>
2024-09-18 21:30:45 +0100

    stat: starting in entities's attributes

--- src/main/java/cuisine/entities/Composant.java
+++ src/main/java/cuisine/entities/Composant.java
@@ -1,4 +1,5 @@
 package cuisine.entities;
 
 public abstract class Composant {
+
 }
--- src/main/java/cuisine/entities/Projet.java
+++ src/main/java/cuisine/entities/Projet.java
@@ -1,4 +1,13 @@
 package cuisine.entities;
 
+import cuisine.entities.Enums.EtatProjet;
+
 public class Projet {
+    private int id;
+    private static int id_starter;
+    private String nom;
+    private double marge_benefique;
+    private double cout_total;
+    private Enum<EtatProjet> etat_projet;
+
 }
