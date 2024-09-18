e49fe4cc9c4c33b99cd8f9e0c70f827663b7a1ae
zoolotoui <chag.med.amine@gmail.com>
2024-09-18 16:19:34 +0100

    update: creating database and constraints

--- script.sql
+++ script.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE BATICUISINE
-       /*creating devis table*/
+    /*creating devis table*/
 Create table devis (
                        id serial Primary key not null,
                        MontantEstime double precision,
@@ -7,3 +7,27 @@ Create table devis (
                        Date_Expiration date,
                        Accept boolean default false
 );
+    /*creating projets table*/
+create table projets (
+                         id serial primary key not null,
+                         nom varchar(255),
+                         marge_bénèfique double precision,
+                         cout_total double precision,
+                         etat varchar(255)
+);
+    /*creating clients table*/
+create table client (
+                        id serial primary key not null,
+                        nom_complet varchar(255),
+                        adresse varchar(255),
+                        telephone varchar(13),
+                        est_pro boolean default false
+);
+    /*creating composants table*/
+create table composants (
+                            id serial primary key not null,
+                            nom varchar(255),
+                            Type_composant varchar(255)
+);
+    /*Adding constraint for projets and devis tables*/
+alter table devis add column projet_id integer references projets (id)
