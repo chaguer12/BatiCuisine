346fca5375d8ade40231f621ec4444f5ffef925c
zoolotoui <chag.med.amine@gmail.com>
2024-09-19 09:46:40 +0100

    update: working on Devis entity

--- src/main/java/cuisine/entities/Devis.java
+++ src/main/java/cuisine/entities/Devis.java
@@ -1,4 +1,56 @@
 package cuisine.entities;
 
+import java.sql.Date;
+
 public class Devis {
+    private static int id_starter = 0;
+    private int id;
+    private double montant_estime;
+    private Date date_emission;
+    private Date date_expiration;
+    private boolean accepte;
+    public Devis(int id,double montant_estime, Date date_emission, Date date_expiration, boolean accepte) {
+        this.id = id_starter++;
+        this.montant_estime = montant_estime;
+        this.date_emission = date_emission;
+        this.date_expiration = date_expiration;
+        this.accepte = accepte;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+
+    public double getMontant_estime() {
+        return montant_estime;
+    }
+
+    public void setMontant_estime(double montant_estime) {
+        this.montant_estime = montant_estime;
+    }
+
+    public Date getDate_emission() {
+        return date_emission;
+    }
+
+    public void setDate_emission(Date date_emission) {
+        this.date_emission = date_emission;
+    }
+
+    public Date getDate_expiration() {
+        return date_expiration;
+    }
+
+    public void setDate_expiration(Date date_expiration) {
+        this.date_expiration = date_expiration;
+    }
+
+    public boolean isAccepte() {
+        return accepte;
+    }
+
+    public void setAccepte(boolean accepte) {
+        this.accepte = accepte;
+    }
 }
